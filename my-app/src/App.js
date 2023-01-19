59bd9370bd6d057771e783ea3fdd0c70caaa3f9b
sourabhpatel073 <sourabhpatel073@email.com>
2023-01-19 12:12:08 +0530

    day2

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      bestdeal
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
